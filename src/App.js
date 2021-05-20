bb1fe11c2e1108ee78d00f28308ef634874ab32a
ankittailor <tailor.akki1011@gmail.com>
2021-05-21 00:09:26 +0530

    change import paths: Home, SearchPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home/Home";
-import SearchPage from "./components/SearchPage";
+import Home from "./pages/Home/Home";
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
   return (
