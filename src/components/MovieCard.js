4f8a0a1d271ba71da89639078997847785849c4f
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 00:16:51 +0530

    add Link

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./MovieCard.css";
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorderRounded";
+import { Link } from "react-router-dom";
 
-function MovieCard({ posterUrl, title }) {
+function MovieCard({ movieId, posterUrl, title }) {
   return (
     <div className="movieCard">
       <div className="movieCard__details">
-        <img src={posterUrl} alt="movie_image" />
+        <Link
+          style={{ textDecoration: "none", color: "black" }}
+          to={`/movie/${movieId}`}
+        >
+          <img src={posterUrl} alt="movie_image" />
+        </Link>
         <div className="movieCard__bookmark">
-          <p>{title}</p>
+          <Link
+            style={{ textDecoration: "none", color: "black" }}
+            to={`/movie/${movieId}`}
+          >
+            <p style={{ cursor: "pointer" }}>
+              {title.length > 30 ? `${title.substr(0, 25)}...` : title}
+            </p>
+          </Link>
           <BookmarkBorderIcon />
         </div>
       </div>
