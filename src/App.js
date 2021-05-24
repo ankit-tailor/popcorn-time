faf6e001c1ca6a735dd49e44c7b6413a741314a4
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 02:55:50 +0530

    add rote

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./components/Header";
 import Home from "./pages/Home/Home";
 import Movie from "./pages/Movie/Movie";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import TopRated from "./pages/TopRated/TopRated";
+import Trending from "./pages/Trending/Trending";
+import Upcoming from "./pages/Upcoming/Upcoming";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/search/:searchTerm" exact component={SearchPage} />
           <Route path="/movie/:movieId" exact component={Movie} />
+          <Route path="/trending" exact component={Trending} />
+          <Route path="/upcoming" exact component={Upcoming} />
+          <Route path="/toprated" exact component={TopRated} />
         </Switch>
       </Router>
     </div>
