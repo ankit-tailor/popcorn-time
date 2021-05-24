6d5d01ea2937acec7e96637d9e2cf43dec9dd46f
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 02:58:28 +0530

    add page_no

--- src/hooks/useFetchHome.js
+++ src/hooks/useFetchHome.js
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
 import { API, baseUrl } from "../backend";
 import axios from "axios";
-const useFetchHome = (endPointUrl) => {
+const useFetchHome = (endPointUrl, page_no) => {
   const [data, setData] = useState([]);
+  const [totalPages, setTotalPages] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [bannerInfo, setBannerInfo] = useState({
     title: "",
     release_date: "",
     backdrop_path: "",
   });
+  // console.log(API);
   useEffect(() => {
     (async () => {
+      setLoading(true);
       let trendingMovies = await axios.get(`${baseUrl}${endPointUrl}`, {
         params: {
           api_key: API,
+          page: page_no,
         },
       });
       const {
-        data: { results },
+        data: { results, total_pages },
       } = trendingMovies;
       let { backdrop_path, title, release_date, id } =
         results[Math.floor(Math.random() * 20)];
       setData(results);
+      setTotalPages(total_pages);
       setBannerInfo({
         title,
         release_date,
         backdrop_path,
         id,
       });
+      setLoading(false);
     })();
-  }, [endPointUrl]);
+  }, [endPointUrl, page_no]);
   return {
     data,
     bannerInfo,
+    totalPages,
+    loading,
   };
 };
 
