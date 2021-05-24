3ec988bc7aac2035d9c162490412d08a23c032f1
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 00:20:52 +0530

    add Link

--- src/components/SearchMovieCard.js
+++ src/components/SearchMovieCard.js
@@ -1,18 +1,31 @@
 import React from "react";
 import "./SearchMovieCard.css";
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorderRounded";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
-const SearchMovieCard = ({ posterUrl, title }) => {
+const SearchMovieCard = ({ movieId, posterUrl, title }) => {
   return (
-    <div className="searchMovieCard">
+    <motion.div
+      className="searchMovieCard"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <div className="searchMovieCard__details">
-        <img src={posterUrl} alt="movie_image" />
+        <Link
+          style={{ textDecoration: "none", marginBottom: "-1.5rem" }}
+          to={`/movie/${movieId}`}
+        >
+          <img className="posterImage" src={posterUrl} alt="movie_image" />
+        </Link>
         <div className="searchMovieCard__bookmark">
-          <p>{title}</p>
+          <Link style={{ textDecoration: "none" }} to={`/movie/${movieId}`}>
+            <p className="movie__title">{title}</p>
+          </Link>
           <BookmarkBorderIcon />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
