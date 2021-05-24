b8c80d49053e075aa9dd091206b456bb0d294f83
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 00:22:48 +0530

    add recommended and actos

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -6,29 +6,32 @@ const useSearch = (query, page_no) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [totalPages, settotalPages] = useState(0);
 
   useEffect(() => {
     (async () => {
       if (query !== "") {
         setLoading(true);
-        setError(true);
+        setError(false);
+        const searchResult = await axios.get(`${baseUrl}/search/movie`, {
+          params: {
+            api_key: API,
+            query: query,
+            include_adult: true,
+            page: page_no,
+          },
+        });
+        if (searchResult) {
+          setLoading(false);
+        }
+        const {
+          data: { results, total_pages },
+        } = searchResult;
+        // console.log(results);
+        // console.log(searchResult.data);
+        setData(results);
+        settotalPages(total_pages);
       }
-      const searchResult = await axios.get(`${baseUrl}/search/movie`, {
-        params: {
-          api_key: API,
-          query: query,
-          include_adult: true,
-          page: page_no,
-        },
-      });
-      if (searchResult) {
-        setLoading(false);
-      }
-      const {
-        data: { results },
-      } = searchResult;
-      // console.log(results);
-      setData(results);
     })();
   }, [query, page_no]);
 
@@ -36,6 +39,7 @@ const useSearch = (query, page_no) => {
     data,
     loading,
     error,
+    totalPages,
   };
 };
 
