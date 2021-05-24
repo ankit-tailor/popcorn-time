3749b41fa1917fdeedac60a252d5e0ae9f98c1e4
ankit-tailor <tailorankit3345@gmail.com>
2021-05-25 00:24:29 +0530

    add movie route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
 import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
@@ -11,9 +12,8 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/search/:searchTerm">
-            <SearchPage />
-          </Route>
+          <Route path="/search/:searchTerm" exact component={SearchPage} />
+          <Route path="/movie/:movieId" exact component={Movie} />
         </Switch>
       </Router>
     </div>
